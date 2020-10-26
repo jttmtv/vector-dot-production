1f5ba7456325e59103da9d861e9fc26e0e3a5a56
jttmtv <52265704+jttmtv@users.noreply.github.com>
2020-10-26 13:28:06 +0800

    Update upgrade2.cpp

--- upgrade2.cpp
+++ upgrade2.cpp
@@ -1,21 +1,22 @@
+#include <immintrin.h>//Need to include header files
 float simd_dot(const float* x, const float* y, const size_t& len) {
 	float inner_prod = 0.0f;
-	__m256 X, Y; //声明两个存放在SSE的256位专用寄存器的变量
-	__m256 acc = _mm256_setzero_ps(); // 声明一个存放在SSE的256位专用寄存器的变量，用来存放X+Y的结果，初始值为0
-	float sum[8] = { 0 };//存放中间变量的参数
+	__m256 X, Y; //Declare two variables stored in the 256-bit special register of SSE
+	__m256 acc = _mm256_setzero_ps(); //Declare a variable stored in a 256-bit special register of SSE to store the result of X+Y, the initial value is 0
+	float sum[8] = { 0 };//Parameters for storing intermediate variables
 
 	size_t i;
-	for (i = 0; i + 8 < len; i += 8) {//256位专用寄存器，一次性可以处理8组32位变量的运算
-		X = _mm256_load_ps(x + i); // 将x加载到X（由于256位可以存放8个32位数据，所以默认一次加载连续的8个参数）
-		Y = _mm256_load_ps(y + i);//同上
-		acc = _mm256_add_ps(acc, _mm256_mul_ps(X, Y));//x*y，每轮的x1*y1求和，x2*y2求和，x3*y3求和，x4*y4求和，x5*y5求和，x6*y6求和，x7*y7求和，x8*y8求和，最终产生的8个和，放在acc的256位寄存器中。
+	for (i = 0; i + 8 < len; i += 8) {//256-bit special register, which can handle 8 groups of 32-bit variable operations at one time
+		X = _mm256_load_ps(x + i); //Load x into X (because 256 bits can store 8 32-bit data, so by default, load 8 consecutive parameters at a time)
+		Y = _mm256_load_ps(y + i);//Same as above
+		acc = _mm256_add_ps(acc, _mm256_mul_ps(X, Y));//x*y, sum x1*y1 in each round, sum x2*y2, sum x3*y3, sum x4*y4, sum x5*y5, sum x6*y6, sum x7*y7, x8 *y8 sum, the final 8 sums are placed in the 256-bit register of acc.
 	}
-	_mm256_store_ps(sum, acc); // 将acc中的8个32位的数据加载进内存
-	inner_prod = sum[0] + sum[1] + sum[2] + sum[3] + sum[4] + sum[5] + sum[6] + sum[7];//点乘求和
+	_mm256_store_ps(sum, acc); //Load 8 32-bit data in acc into memory
+	inner_prod = sum[0] + sum[1] + sum[2] + sum[3] + sum[4] + sum[5] + sum[6] + sum[7];//Dot product sum
 
-	// 刚才只是处理了前8的倍数个元素的点乘，如果len不是8的倍数，那么还有尾部要处理
+	//Just deal with the dot multiplication of elements that are multiples of the first 8. If len is not a multiple of 8, then there is a tail to be processed
 	for (; i < len; ++i) {
-		inner_prod += x[i] * y[i];//继续累加尾部的乘积
+		inner_prod += x[i] * y[i];//Continue to accumulate the tail product
 	}
 	return inner_prod;
 }
