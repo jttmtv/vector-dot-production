d247ac1c3d7293d3ea69370c0c99f711dba94fd4
jttmtv <52265704+jttmtv@users.noreply.github.com>
2020-10-26 13:52:19 +0800

    Update upgrade1.cpp

--- upgrade1.cpp
+++ upgrade1.cpp
@@ -1,7 +1,7 @@
-inline float vector_dot_product(float* v1, float* v2, int n)
+inline float vector_dot_product(float* v1, float* v2, size_t n)
 {
 	float sum = 0.0f;
-	int a, b;
+	size_t a, b;
 	a = n % 8;
 	b = (n - a) / 8;
 	for (size_t i = 0; i < n; i += 8)
@@ -15,7 +15,7 @@ inline float vector_dot_product(float* v1, float* v2, int n)
 		sum += (v1[i + 6] * v2[i + 6]);
 		sum += (v1[i + 7] * v2[i + 7]);
 	}
-	for (int i = n - 1; i > n - 1 - a; i--)
+	for (size_t i = n - 1; i > n - 1 - a; i--)
 		sum += v1[i] * v2[i];
 	return sum;
 }
